0e6a653daf3f6bd1f9eb99e1288f909c6f277d56
Lihoradit <108638779+Lihoradit@users.noreply.github.com>
2022-12-24 00:07:01 +0300

    Метод вывода изначального массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Введите слова через пробел:");
-string user_str=Console.ReadLine();
-string[] result= user_str.Split(' ');
+﻿string[] array=new string[5]{"123","Ракета","=)","$!@","шлепа"};
+ShowString(array);
+void ShowString(string[] array)
+{
+    Console.WriteLine("Изначальный массив строк:");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        Console.Write($"{array[i]}");
+        if(i+1<array.Length) Console.Write(",");
+    }
+    Console.Write("]");
+}
