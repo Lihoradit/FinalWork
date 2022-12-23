b4278d3b178138b46f18894180dd50e787d5f7ab
Lihoradit <108638779+Lihoradit@users.noreply.github.com>
2022-12-24 00:29:35 +0300

    Завершенный код

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
-﻿string[] array=new string[5]{"123","Ракета","=)","$!@","шлепа"};
+﻿string[] array = new string[5] { "13", "Ракета", "=)", "$!@", "ша" };
+Console.WriteLine("Изначальный массив строк:");
 ShowString(array);
 void ShowString(string[] array)
 {
-    Console.WriteLine("Изначальный массив строк:");
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        
+
         Console.Write($"{array[i]}");
-        if(i+1<array.Length) Console.Write(",");
+        if (i + 1 < array.Length) Console.Write(",");
     }
-    Console.Write("]");
+    Console.WriteLine("]");
 }
-for (int i = 0; i < array.Length; i++)
+Console.WriteLine("Новый массив строк, где меньше 4 символов :");
+string[] NewArray(string[] array)
 {
-    if (array[i].Length<=3) Console.Write(array[i]);
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    string[] newArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 2)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
 }
+ShowString(NewArray(array));
