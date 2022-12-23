9476d6ebbeb22289d99119e29670cc2ab22b002d
Lihoradit <108638779+Lihoradit@users.noreply.github.com>
2022-12-23 23:42:25 +0300

    main code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите слова через пробел:");
+string user_str=Console.ReadLine();
+string[] result= user_str.Split(' ');
